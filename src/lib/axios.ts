5344edcb23a233078a8f45413be430cbba50adde
DevCAI0 <caioribeiro6789@gmail.com>
2024-06-03 22:10:46 -0300

    mudando a url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: 'https://cco-api-beta.onrender.com/',
   withCredentials: true,
 })
 
