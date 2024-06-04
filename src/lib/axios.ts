5989f016c08e6b4c9c9ec7b5331962fdca4c952a
DevCAI0 <caioribeiro6789@gmail.com>
2024-06-03 22:19:01 -0300

    Correçao do package.json

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-  baseURL: 'https://cco-api-beta.onrender.com/',
+  baseURL: 'https://cco-api-beta.onrender.com',
   withCredentials: true,
 })
 
